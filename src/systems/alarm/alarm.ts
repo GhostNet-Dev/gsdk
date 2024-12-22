18f1077b9e18266a3efadf17302288e9dd33f7bb
zcbm4321 <zcbm4321@gmail.com>
2024-12-23 07:01:01 +0900

    alarm refec

--- src/systems/alarm/alarm.ts
+++ src/systems/alarm/alarm.ts
@@ -1,3 +1,5 @@
+import IEventController from "@Glibs/interface/ievent"
+import { EventTypes } from "@Glibs/types/globaltypes"
 import { gsap } from "gsap"
 
 export enum AlarmType {
@@ -9,11 +11,21 @@ export enum AlarmType {
 export class Alarm {
     dom = document.createElement("div")
     bigDom = document.createElement("div")
-    constructor() {
+    constructor(eventCtrl: IEventController) {
         this.dom.className = "playalarm"
+        this.makeDomStyle(this.dom)
         document.body.appendChild(this.dom)
+
         this.bigDom.className = "bigplayalarm"
+        this.makeDomStyle(this.bigDom)
         document.body.appendChild(this.bigDom)
+
+        eventCtrl.RegisterEventListener(EventTypes.AlarmNormal, (text: string) => {
+            this.NotifyInfo(text, AlarmType.Normal)
+        })
+        eventCtrl.RegisterEventListener(EventTypes.AlarmWarning, (text: string) => {
+            this.NotifyInfo(text, AlarmType.Warning)
+        })
     }
     NotifyInfo(text: string, type: AlarmType) {
         switch(type) {
@@ -27,6 +39,7 @@ export class Alarm {
                     }
                 })
                 break;
+            case AlarmType.Warning:
             case AlarmType.Deck:
                 this.bigDom.style.display = "block"
                 this.bigDom.insertAdjacentHTML("beforeend", text + "<br>")
@@ -38,5 +51,17 @@ export class Alarm {
                 })
                 break;
         }
+    }
+    makeDomStyle(dom: HTMLElement) {
+        // 스타일 설정
+        dom.style.position = "absolute";
+        dom.style.display = "none";
+        dom.style.width = "fit-content";
+        dom.style.top = "15%";
+        dom.style.left = "50%";
+        dom.style.transform = "translate(-50%, -50%)";
+        dom.style.color = "#fff";
+        dom.style.textShadow = "-1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black";
+        dom.style.textAlign = "center";
     }
 }
